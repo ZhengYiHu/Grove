034cd9c5e010ecb4dab39d7e0174cf912eaca055
Zy <zhengyi.hu.98@gmail.com>
2023-02-20 21:54:20 +0000

    chore: imported lean tween and round icons. Removed assets

--- Assets/Scripts/HoverToScale.cs
+++ Assets/Scripts/HoverToScale.cs
@@ -1,17 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class HoverToScale : MonoBehaviour
+public class HoverToScale : MonoBehaviour, IPointerEnterHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter(PointerEventData eventData)
     {
         
     }
