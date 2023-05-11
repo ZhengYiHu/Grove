5a3a18a924cafc8501dc07abd895d396e12efb0b
Zy <zhengyi.hu.98@gmail.com>
2023-05-12 00:35:00 +0100

    Revert "test: debug test build"
    This reverts commit cffded3f629d2e37980823c3146a651232447108.

--- Assets/Scripts/VideoPathSetter.cs
+++ Assets/Scripts/VideoPathSetter.cs
@@ -13,17 +13,13 @@ public class VideoPathSetter : MonoBehaviour
     [SerializeField] VideoPlayer videoPlayer;
     [SerializeField] VideoClip videoClip;
     [SerializeField] TextMeshProUGUI text;
-    [SerializeField] TextMeshProUGUI text2;
-    [SerializeField] TextMeshProUGUI text3;
     void Start()
     {
         if(videoPlayer == null)
         {
             videoPlayer = GetComponent<VideoPlayer>();
         }
-        text2.text = $"videoClip null: {videoClip == null}";
-        text3.text = $"Name: {videoClip.name}";
-        text.text = $"{Application.streamingAssetsPath}/{videoClip.name}.mp4";
+        text.text = System.IO.Path.Combine(Application.streamingAssetsPath, videoClip.name) + ".mp4";
 #if UNITY_EDITOR
         videoPlayer.source = VideoSource.VideoClip;
         videoPlayer.url = AssetDatabase.GetAssetPath(videoClip);
