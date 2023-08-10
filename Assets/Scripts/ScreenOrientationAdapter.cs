7ecfea1917ac191bf8bc3b2f46b019d3e77639f1
Zheng Yi <zhengyi.hu.98@gmail.com>
2023-08-10 17:10:26 +0200

    Chore: removed 3d scene and references

--- Assets/Scripts/ScreenOrientationAdapter.cs
+++ Assets/Scripts/ScreenOrientationAdapter.cs
@@ -6,18 +6,9 @@ using UnityEngine;
 public class ScreenOrientationAdapter : MonoBehaviour
 {
     static public bool isLandscape;
-    [SerializeField] ScenePreview scenePreviewLandscape;
-    [SerializeField] ScenePreview scenePreviewVertical;
-
-    static public ScenePreview preview;
-    static public ScenePreview disabledPreview;
 
     private void Update()
     {
         isLandscape = Screen.width > Screen.height;
-        scenePreviewLandscape.gameObject.SetActive(isLandscape);
-        scenePreviewVertical.gameObject.SetActive(!isLandscape);
-        preview = isLandscape ? scenePreviewLandscape : scenePreviewVertical;
-        disabledPreview = isLandscape ? scenePreviewVertical : scenePreviewLandscape;
     }
 }
